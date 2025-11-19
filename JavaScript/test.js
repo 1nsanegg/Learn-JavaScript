79881293f2ceb87eb9efac16346b3a09fbdb8061
1nsanegg <2001040218@s.hanu.edu.vn>
2025-11-19 08:53:03 +0700

    practice js: rendering html elements, create function

---
+++ JavaScript/test.js
@@ -0,0 +1,19 @@
+// let firstName = "Trevor";
+// let lastName = "Khuat";
+
+// function greeting() {
+//   let fullName = firstName + " " + lastName;
+//   console.log(fullName);
+// }
+// greeting()
+
+let myPoint = 3
+function add3Point() {
+    myPoint+=3
+}
+function remove1Point() {
+    myPoint-=1
+}
+add3Point()
+remove1Point()
+console.log(myPoint)
