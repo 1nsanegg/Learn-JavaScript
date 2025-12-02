67ce8a8e060d879fc6c86532132753ec1e0fdaaa
Trevor Khuat <107063052+1nsanegg@users.noreply.github.com>
2025-12-02 19:56:46 +0700

    basic javascript

---
+++ JavaScript/Objects and functions/index.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Trevor",
+    age: 23,
+    country: "Vietnam"
+}
+
+function logData(person) {
+    console.log(`${person.name} is ${person.age} years old and lives in ${person.country}`)
+}
+
+logData(person)
---
+++ JavaScript/push, pop, unshift, shift/index.js
@@ -0,0 +1,9 @@
+let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"];
+
+largeCountries.shift();
+largeCountries.unshift("China");
+
+largeCountries.pop();
+largeCountries.push("Pakistan");
+
+console.log(largeCountries);
